7c7d4e899b857ff3836994e6e524a5e5fb99e44f
Malcolm Campbell <malcgcamp@gmail.com>
2020-06-24 17:41:35 -0400

    added regression to remove effect of time in session

--- malcolm/one_reward_patches.m
+++ malcolm/one_reward_patches.m
@@ -12,8 +12,8 @@ addpath(genpath('C:\code\patch_foraging_neuropix\malcolm\functions'));
 
 % analysis options
 opt = struct;
-opt.session = '80_20200317'; % session to analyze
-opt.cellid = 368; % example cell to plot
+opt.session = '80_20200315'; % session to analyze
+% opt.cellid = 368; % example cell to plot
 opt.tbin = 0.02; % time bin for whole session rate matrix (in sec)
 opt.smoothSigma_time = 0.1; % gauss smoothing sigma for rate matrix (in sec)
 
@@ -58,97 +58,315 @@ end
 one_rew_patches = find(dat.patches(:,3)==1 & mod(dat.patches(:,2),10)==4);
 prt = dat.patches(one_rew_patches,5);
 
-%% plot pc1 trace for these patches
+%% regress out time in session and time^2 to remove slowly varying changes
+
+X = [ones(size(tbincent)); tbincent; tbincent.^2]'; % include time
+beta = X\score;
+score_resid = score-X*beta;
+beta2 = X\score_resid;
+
+hfig = figure('Position',[200 200 600 1000]);
+hfig.Name = sprintf('one_rew_patches_%s_PCs1-6 original and residual after removing time and time squared',opt.session);
+counter = 1;
+for i = 1:6
+    subplot(6,2,counter); hold on;
+    counter = counter+1;
+    my_scatter(tbincent,score(:,i),'k',0.1);
+    plot(tbincent,X*beta(:,i),'r-','LineWidth',2);
+    title(sprintf('PC%d: ORIGINAL',i));
+    xlabel('time (sec)');
+    ylabel('PC');
+    
+    subplot(6,2,counter); hold on;
+    counter = counter+1;
+    my_scatter(tbincent,score_resid(:,i),'k',0.1);
+    plot(tbincent,X*beta2(:,i),'r-','LineWidth',2);
+    title(sprintf('PC%d: RESIDUAL',i));
+    xlabel('time (sec)');
+    ylabel('PC (residual)');
+end
+
+save_figs(paths.figs,hfig,'png');
+
+%% plot pc traces for these patches: ORIGINAL
 
 T = ceil(max(prt));
 t = 0:opt.tbin:T;
 N = numel(t);
-pca_one_rew_patches = nan(numel(one_rew_patches),N);
-for i = 1:numel(one_rew_patches)
-    pca_this = score(patch_num==one_rew_patches(i),1);
-    % pca_this;
-    pca_one_rew_patches(i,1:numel(pca_this)) = pca_this;
-end
 
-% sort by prt
-[~,sort_idx] = sort(prt);
-pca_one_rew_patches_sort = pca_one_rew_patches(sort_idx,:);
+hfig = figure('Position',[100 300 2100 800]); hold on;
+hfig.Name = sprintf('one_rew_patches_%s_PCs1-6 - 4 uL patches - only reward at t=0',opt.session);
 
-% plot
-hfig = figure('Position',[300 300 600 500]); hold on;
-hfig.Name = sprintf('one_rew_patches_%s_PC1',opt.session);
+% plot PRTs vs patch number in session
+subplot(2,7,1); hold on;
 plot_col = cool(numel(one_rew_patches));
+[prt_sorted,sort_idx] = sort(prt);
+one_rew_patches_sorted = one_rew_patches(sort_idx);
 for i = 1:numel(one_rew_patches)
-    y_this = pca_one_rew_patches_sort(i,:);
-    lh = plot(t,y_this);
-    lh.Color = [plot_col(i,:) 0.8];
-    max_idx = find(~isnan(y_this),1,'last');
-    my_scatter(t(max_idx),y_this(max_idx),plot_col(i,:),0.5);
+    my_scatter(one_rew_patches_sorted(i),prt_sorted(i),plot_col(i,:),0.5);
 end
-xlabel('Time on patch (sec)');
-ylabel('PC1')
-title(sprintf('%s\n4 uL patches, only reward at t=0',opt.session),'Interpreter','none')
-plot([min(prt) min(prt)],ylim,'k--');
+ylabel('PRT (sec)');
+xlabel('Patch Num');
+axis square;
 
-% plot significance
-for i = 1:size(pca_one_rew_patches,2)
-    keep = ~isnan(pca_one_rew_patches(:,i));
-    y_this = pca_one_rew_patches(keep,i);
-    prt_this = prt(keep);
-    if numel(y_this)>1
-        [r,p] = corrcoef(y_this,prt_this);
-        if p(1,2)<0.05
-            my_scatter(t(i),max(ylim)-0.5,'k',1);
+% plot PCs
+for pc_num = 1:6
+    pca_one_rew_patches = nan(numel(one_rew_patches),N);
+    for i = 1:numel(one_rew_patches)
+        pca_this = score(patch_num==one_rew_patches(i),pc_num);
+        pca_one_rew_patches(i,1:numel(pca_this)) = pca_this;
+    end
+
+    % sort by prt
+    [~,sort_idx] = sort(prt);
+    pca_one_rew_patches_sort = pca_one_rew_patches(sort_idx,:);
+
+    % plot
+    subplot(2,7,pc_num+1); hold on;
+    plot_col = cool(numel(one_rew_patches));
+    for i = 1:numel(one_rew_patches)
+        y_this = pca_one_rew_patches_sort(i,:);
+        lh = plot(t,y_this);
+        lh.Color = [plot_col(i,:) 0.8];
+        max_idx = find(~isnan(y_this),1,'last');
+        my_scatter(t(max_idx),y_this(max_idx),plot_col(i,:),0.5);
+    end
+    xlabel('Time on patch (sec)');
+    ylabel('PC')
+    % title(sprintf('%s\n4 uL patches, only reward at t=0',opt.session),'Interpreter','none')
+    title(sprintf('PC%d',pc_num));
+    plot([min(prt) min(prt)],ylim,'k--');
+    axis square;
+
+    % plot significance
+    for i = 1:size(pca_one_rew_patches,2)
+        keep = ~isnan(pca_one_rew_patches(:,i));
+        y_this = pca_one_rew_patches(keep,i);
+        prt_this = prt(keep);
+        if numel(y_this)>1
+            [r,p] = corrcoef(y_this,prt_this);
+            if p(1,2)<0.05
+                plot(t(i),max(ylim)-0.5,'k.');
+            end
         end
     end
+       
+end
+
+% SAME but colored by patch number (proxy for time in session)
+% plot PRTs vs patch number in session
+subplot(2,7,8); hold on;
+plot_col = autumn(numel(one_rew_patches));
+for i = 1:numel(one_rew_patches)
+    my_scatter(one_rew_patches(i),prt(i),plot_col(i,:),0.5);
+end
+ylabel('PRT (sec)');
+xlabel('Patch Num');
+axis square;
+
+% plot PCs
+for pc_num = 1:6
+    pca_one_rew_patches = nan(numel(one_rew_patches),N);
+    for i = 1:numel(one_rew_patches)
+        pca_this = score(patch_num==one_rew_patches(i),pc_num);
+        pca_one_rew_patches(i,1:numel(pca_this)) = pca_this;
+    end
+
+    % plot
+    subplot(2,7,7+pc_num+1); hold on;
+    plot_col = autumn(numel(one_rew_patches));
+    for i = 1:numel(one_rew_patches)
+        y_this = pca_one_rew_patches(i,:);
+        lh = plot(t,y_this);
+        lh.Color = [plot_col(i,:) 0.8];
+        max_idx = find(~isnan(y_this),1,'last');
+        my_scatter(t(max_idx),y_this(max_idx),plot_col(i,:),0.5);
+    end
+    xlabel('Time on patch (sec)');
+    ylabel('PC')
+    % title(sprintf('%s\n4 uL patches, only reward at t=0',opt.session),'Interpreter','none')
+    title(sprintf('PC%d',pc_num));
+    plot([min(prt) min(prt)],ylim,'k--');
+    axis square;
+
+    % plot significance
+    for i = 1:size(pca_one_rew_patches,2)
+        keep = ~isnan(pca_one_rew_patches(:,i));
+        y_this = pca_one_rew_patches(keep,i);
+        x_this = one_rew_patches(keep);
+        if numel(y_this)>1
+            [r,p] = corrcoef(y_this,x_this);
+            if p(1,2)<0.05
+                plot(t(i),max(ylim)-0.5,'k.');
+            end
+        end
+    end
+       
+end
+
+save_figs(paths.figs,hfig,'png');
+
+%% plot pc traces for these patches: TIME AND TIME^2 REGRESSED OUT
+
+T = ceil(max(prt));
+t = 0:opt.tbin:T;
+N = numel(t);
+
+hfig = figure('Position',[100 300 2100 800]); hold on;
+hfig.Name = sprintf('one_rew_patches_%s_PCs1-6 - 4 uL patches - only reward at t=0 - regressed out time in session and time squared',opt.session);
+
+% plot PRTs vs patch number in session
+subplot(2,7,1); hold on;
+plot_col = cool(numel(one_rew_patches));
+[prt_sorted,sort_idx] = sort(prt);
+one_rew_patches_sorted = one_rew_patches(sort_idx);
+for i = 1:numel(one_rew_patches)
+    my_scatter(one_rew_patches_sorted(i),prt_sorted(i),plot_col(i,:),0.5);
+end
+ylabel('PRT (sec)');
+xlabel('Patch Num');
+axis square;
+
+% plot PCs
+for pc_num = 1:6
+    pca_one_rew_patches = nan(numel(one_rew_patches),N);
+    for i = 1:numel(one_rew_patches)
+        pca_this = score_resid(patch_num==one_rew_patches(i),pc_num);
+        pca_one_rew_patches(i,1:numel(pca_this)) = pca_this;
+    end
+
+    % sort by prt
+    [~,sort_idx] = sort(prt);
+    pca_one_rew_patches_sort = pca_one_rew_patches(sort_idx,:);
+
+    % plot
+    subplot(2,7,pc_num+1); hold on;
+    plot_col = cool(numel(one_rew_patches));
+    for i = 1:numel(one_rew_patches)
+        y_this = pca_one_rew_patches_sort(i,:);
+        lh = plot(t,y_this);
+        lh.Color = [plot_col(i,:) 0.8];
+        max_idx = find(~isnan(y_this),1,'last');
+        my_scatter(t(max_idx),y_this(max_idx),plot_col(i,:),0.5);
+    end
+    xlabel('Time on patch (sec)');
+    ylabel('PC')
+    % title(sprintf('%s\n4 uL patches, only reward at t=0',opt.session),'Interpreter','none')
+    title(sprintf('PC%d',pc_num));
+    plot([min(prt) min(prt)],ylim,'k--');
+    axis square;
+
+    % plot significance
+    for i = 1:size(pca_one_rew_patches,2)
+        keep = ~isnan(pca_one_rew_patches(:,i));
+        y_this = pca_one_rew_patches(keep,i);
+        prt_this = prt(keep);
+        if numel(y_this)>1
+            [r,p] = corrcoef(y_this,prt_this);
+            if p(1,2)<0.05
+                plot(t(i),max(ylim)-0.5,'k.');
+            end
+        end
+    end
+       
+end
+
+% SAME but colored by patch number (proxy for time in session)
+% plot PRTs vs patch number in session
+subplot(2,7,8); hold on;
+plot_col = autumn(numel(one_rew_patches));
+for i = 1:numel(one_rew_patches)
+    my_scatter(one_rew_patches(i),prt(i),plot_col(i,:),0.5);
+end
+ylabel('PRT (sec)');
+xlabel('Patch Num');
+axis square;
+
+% plot PCs
+for pc_num = 1:6
+    pca_one_rew_patches = nan(numel(one_rew_patches),N);
+    for i = 1:numel(one_rew_patches)
+        pca_this = score_resid(patch_num==one_rew_patches(i),pc_num);
+        pca_one_rew_patches(i,1:numel(pca_this)) = pca_this;
+    end
+
+    % plot
+    subplot(2,7,7+pc_num+1); hold on;
+    plot_col = autumn(numel(one_rew_patches));
+    for i = 1:numel(one_rew_patches)
+        y_this = pca_one_rew_patches(i,:);
+        lh = plot(t,y_this);
+        lh.Color = [plot_col(i,:) 0.8];
+        max_idx = find(~isnan(y_this),1,'last');
+        my_scatter(t(max_idx),y_this(max_idx),plot_col(i,:),0.5);
+    end
+    xlabel('Time on patch (sec)');
+    ylabel('PC')
+    % title(sprintf('%s\n4 uL patches, only reward at t=0',opt.session),'Interpreter','none')
+    title(sprintf('PC%d',pc_num));
+    plot([min(prt) min(prt)],ylim,'k--');
+    axis square;
+
+    % plot significance
+    for i = 1:size(pca_one_rew_patches,2)
+        keep = ~isnan(pca_one_rew_patches(:,i));
+        y_this = pca_one_rew_patches(keep,i);
+        x_this = one_rew_patches(keep);
+        if numel(y_this)>1
+            [r,p] = corrcoef(y_this,x_this);
+            if p(1,2)<0.05
+                plot(t(i),max(ylim)-0.5,'k.');
+            end
+        end
+    end
+       
 end
 
 save_figs(paths.figs,hfig,'png');
 
 %% plot example cell for these patches
 
-% get firing rate trace for this cell for these patches
-T = ceil(max(prt));
-t = 0:opt.tbin:T;
-N = numel(t);
-fr_one_rew_patches = nan(numel(one_rew_patches),N);
-for i = 1:numel(one_rew_patches)
-    fr_this = fr_mat_in_patch(good_cells==opt.cellid,patch_num==one_rew_patches(i));
-    % pca_this;
-    fr_one_rew_patches(i,1:numel(fr_this)) = fr_this;
-end
-
-% sort by prt
-[~,sort_idx] = sort(prt);
-fr_one_rew_patches_sort = fr_one_rew_patches(sort_idx,:);
-
-% plot
-hfig = figure('Position',[300 300 600 500]); hold on;
-hfig.Name = sprintf('one_rew_patches_%s_c%d',opt.session,opt.cellid);
-plot_col = cool(numel(one_rew_patches));
-for i = 1:numel(one_rew_patches)
-    y_this = fr_one_rew_patches_sort(i,:);
-    lh = plot(t,y_this);
-    lh.Color = [plot_col(i,:) 0.8];
-    max_idx = find(~isnan(y_this),1,'last');
-    my_scatter(t(max_idx),y_this(max_idx),plot_col(i,:),0.5);
-end
-xlabel('Time on patch (sec)');
-ylabel('Firing rate (Hz)')
-title(sprintf('%s\nCELL %d, 4 uL patches, only reward at t=0',opt.session,opt.cellid),'Interpreter','none')
-plot([min(prt) min(prt)],ylim,'k--');
-
-% plot significance
-for i = 1:size(fr_one_rew_patches,2)
-    keep = ~isnan(fr_one_rew_patches(:,i));
-    y_this = fr_one_rew_patches(keep,i);
-    prt_this = prt(keep);
-    if numel(y_this)>1
-        [r,p] = corrcoef(y_this,prt_this);
-        if p(1,2)<0.05
-            my_scatter(t(i),max(ylim)-2,'k',1);
-        end
-    end
-end
-
-save_figs(paths.figs,hfig,'png');
+% % get firing rate trace for this cell for these patches
+% T = ceil(max(prt));
+% t = 0:opt.tbin:T;
+% N = numel(t);
+% fr_one_rew_patches = nan(numel(one_rew_patches),N);
+% for i = 1:numel(one_rew_patches)
+%     fr_this = fr_mat_in_patch(good_cells==opt.cellid,patch_num==one_rew_patches(i));
+%     fr_one_rew_patches(i,1:numel(fr_this)) = fr_this;
+% end
+% 
+% % sort by prt
+% [~,sort_idx] = sort(prt);
+% fr_one_rew_patches_sort = fr_one_rew_patches(sort_idx,:);
+% 
+% % plot
+% hfig = figure('Position',[300 300 600 500]); hold on;
+% hfig.Name = sprintf('one_rew_patches_%s_c%d',opt.session,opt.cellid);
+% plot_col = cool(numel(one_rew_patches));
+% for i = 1:numel(one_rew_patches)
+%     y_this = fr_one_rew_patches_sort(i,:);
+%     lh = plot(t,y_this);
+%     lh.Color = [plot_col(i,:) 0.8];
+%     max_idx = find(~isnan(y_this),1,'last');
+%     my_scatter(t(max_idx),y_this(max_idx),plot_col(i,:),0.5);
+% end
+% xlabel('Time on patch (sec)');
+% ylabel('Firing rate (Hz)')
+% title(sprintf('%s\nCELL %d, 4 uL patches, only reward at t=0',opt.session,opt.cellid),'Interpreter','none')
+% plot([min(prt) min(prt)],ylim,'k--');
+% 
+% % plot significance
+% for i = 1:size(fr_one_rew_patches,2)
+%     keep = ~isnan(fr_one_rew_patches(:,i));
+%     y_this = fr_one_rew_patches(keep,i);
+%     prt_this = prt(keep);
+%     if numel(y_this)>1
+%         [r,p] = corrcoef(y_this,prt_this);
+%         if p(1,2)<0.05
+%             my_scatter(t(i),max(ylim)-2,'k',1);
+%         end
+%     end
+% end
+% 
+% save_figs(paths.figs,hfig,'png');
