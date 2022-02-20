6c7e5926f760bc50ca6ee7a383e95cec0db53d22
sternj98 <sternj@college.harvard.edu>
2022-02-20 18:17:19 -0500

    some umap dbscan

--- josh/glm_analysis/clustering_v2/explorative.m
+++ josh/glm_analysis/clustering_v2/explorative.m
@@ -3,48 +3,52 @@
 %% Load data
 
 % paths
-data_path = '/Users/joshstern/Documents/UchidaLab_NeuralData/processed_neuropix_data/glm_20211105_50ms_bins/GLM_coeffs_20211105_50_ms_bins.mat';
-old_clusters_path = '/Users/joshstern/Documents/UchidaLab_NeuralData/processed_neuropix_data/glm_20211105_50ms_bins/GMM_clust_20211105_50_ms_bins.mat';
+% data_path = '/Users/joshstern/Documents/UchidaLab_NeuralData/processed_neuropix_data/glm_20211105_50ms_bins/GLM_coeffs_20211105_50_ms_bins.mat';
+% old_clusters_path = '/Users/joshstern/Documents/UchidaLab_NeuralData/processed_neuropix_data/glm_20211105_50ms_bins/GMM_clust_20211105_50_ms_bins.mat';
+
+data_path = 'D:\patchforaging_data\GLM_coeffs_20220213_50_ms_bins_logDVs_logScaleKern'; 
+waveforms_path = 'D:\patchforaging_data\waveforms'; 
 
 glm_dat = load(data_path);
-old_gmm_dat = load(old_clusters_path); 
-coeffs_to_cluster = cellfun(@(this_var) any(strcmp(this_var,old_gmm_dat.var_name)),glm_dat.var_name);
+rew_kern_mask = cellfun(@(x) strcmp(x(1:3),'Rew'),glm_dat.var_name); 
+time_mask = cellfun(@(x) strcmp(x(1:4),'Time'),glm_dat.var_name); 
+total_rew_mask = cellfun(@(x) strcmp(x(1:5),'Total'),glm_dat.var_name); 
+
+coeffs_to_cluster = any([rew_kern_mask time_mask total_rew_mask],2); 
 
 % pull off friends with nonzero cognitive variables
 any_nonzero_mask = sum(abs(glm_dat.beta_all(coeffs_to_cluster,:))>0)'>0;
+beta_any_nonzero = glm_dat.beta_all(:,any_nonzero_mask); 
 all_nonzero_cellIDs = glm_dat.cellID(any_nonzero_mask);
-pfc_mask = strcmp("PFC",glm_dat.brain_region_rough);
-pfc_nonzero_cellIDs = glm_dat.cellID(pfc_mask & any_nonzero_mask);
+region = glm_dat.brain_region_rough(any_nonzero_mask); 
+depth = glm_dat.depth_from_surface(any_nonzero_mask); 
+pfc_mask = strcmp("PFC",region);
 
-beta_all = glm_dat.beta_all(coeffs_to_cluster,any_nonzero_mask);
-beta_subpfc = glm_dat.beta_all(coeffs_to_cluster,~pfc_mask & any_nonzero_mask);
-beta_pfc = glm_dat.beta_all(coeffs_to_cluster,pfc_mask & any_nonzero_mask);
-region = glm_dat.brain_region_rough(any_nonzero_mask);
+beta_all = beta_any_nonzero(coeffs_to_cluster,:);
+beta_pfc = beta_any_nonzero(coeffs_to_cluster,pfc_mask);
 
-
-%% plot time integration versus avg reward response
+%% Let's try umapping it
+% try across a range of n_neighbors and min distance
+min_dist_test = [.1 .15 .2 .3]; 
+n_neighbors_test = [10 20 30 50]; 
 
 beta_all_norm = zscore(beta_all,[],2);
 beta_pfc_norm = zscore(beta_pfc,[],2);
 
-rew_mask = cellfun(@(x) strcmp(x(1:3),'Rew') && strcmp(x(end-2:end),'4uL') ,old_gmm_dat.var_name); 
-time_mask = cellfun(@(x) strcmp(x(1:4),'Time') && strcmp(x(end-2:end),'4uL'),old_gmm_dat.var_name); 
+n_components = 2;
 
-rew_coeff = mean(beta_pfc_norm(rew_mask,:),1);
-time_coeff = mean(beta_pfc_norm(time_mask,:),1);
 figure()
-binscatter(rew_coeff(abs(rew_coeff) > 0.2),time_coeff(abs(rew_coeff) > 0.2),30,'xlimits',[-2,2],'ylimits',[-2,2])
-ylim([-2,2])
-xlim([-2,2])
-
-%% Let's try umapping it
-
-% n_neighbors = n_neighbors_test(i_n_neighbors);
-n_components = 2; 
-n_neighbors = 50; 
-
-X_umap = run_umap(beta_all_norm','n_components',n_components,'n_neighbors',n_neighbors,'verbose','none');
+for i_n_neighbors = 1:numel(n_neighbors_test)
+    n_neighbors = n_neighbors_test(i_n_neighbors);
+    for i_min_dist = 1:numel(min_dist_test)
+        min_dist = min_dist_test(i_min_dist);
+        X_umap = run_umap(beta_pfc_norm','n_components',n_components,'n_neighbors',n_neighbors,'min_dist',min_dist, 'verbose','none');
+        subplot(numel(min_dist_test),numel(n_neighbors_test),i_n_neighbors + (i_min_dist - 1) * numel(n_neighbors_test))
+        scatter(X_umap(:,1),X_umap(:,2),3,'o')
+%         gscatter(X_umap(:,1),X_umap(:,2),region,[],'o',3,'legend',[])
+    end
+end
 
 %% 
-gscatter(X_umap(:,1),X_umap(:,2),region,[],'o',10)
+gscatter(X_umap(:,1),X_umap(:,2),region,[],'o',3)
 
--- josh/glm_analysis/clustering_v2/hdbscan_exploration.m
+++ josh/glm_analysis/clustering_v2/hdbscan_exploration.m
@@ -2,16 +2,37 @@
 
 %% Load digits data 
 oldpath = addpath(fullfile(matlabroot,'examples','nnet','main'));
-mnist_directory = '/Users/joshstern/Documents/mnist_dataset';
-filenameImagesTrain = 'train-images-idx3-ubyte';
-filenameLabelsTrain = 'train-labels-idx1-ubyte';
-filenameImagesTest = 't10k-images-idx3-ubyte';
-filenameLabelsTest = 't10k-labels-idx1-ubyte';
+mnist_directory = 'C:\Users\joshs\Documents\mnist_data';
+filenameImagesTrain = 'train-images-idx3-ubyte.gz';
+filenameLabelsTrain = 'train-labels-idx1-ubyte.gz';
+filenameImagesTest = 't10k-images-idx3-ubyte.gz';
+filenameLabelsTest = 't10k-labels-idx1-ubyte.gz';
 
-XTrain = processImagesMNIST(fullfile(mnist_directory,filenameImagesTrain));
-
+XTrain = extractdata(processImagesMNIST(fullfile(mnist_directory,filenameImagesTrain)));
 YTrain = processLabelsMNIST(fullfile(mnist_directory,filenameLabelsTrain));
-XTest = processImagesMNIST(fullfile(mnist_directory,filenameImagesTest));
+XTest = extractdata(processImagesMNIST(fullfile(mnist_directory,filenameImagesTest)));
 YTest = processLabelsMNIST(fullfile(mnist_directory,filenameLabelsTest));
 
-function X = read_mnist_image
+XTrain_flat = reshape(XTrain,[28*28,60000]); 
+XTest_flat = reshape(XTest,[28*28,10000]); 
+
+%% Run UMAP 
+
+min_dist_test = [.1 .15 .2 .3]; 
+n_neighbors_test = [10 20 30 50]; 
+
+XTrain_norm = zscore(XTrain_flat'); 
+
+n_components = 2;
+
+figure()
+for i_n_neighbors = 1:numel(n_neighbors_test)
+    n_neighbors = n_neighbors_test(i_n_neighbors);
+    for i_min_dist = 1:numel(min_dist_test)
+        min_dist = min_dist_test(i_min_dist);
+        X_umap = run_umap(XTrain_norm,'n_components',n_components,'n_neighbors',n_neighbors,'min_dist',min_dist, 'verbose','none');
+        subplot(numel(min_dist_test),numel(n_neighbors_test),i_n_neighbors + (i_min_dist - 1) * numel(n_neighbors_test))
+%         scatter(X_umap(:,1),X_umap(:,2),3,'o')
+        gscatter(X_umap(:,1),X_umap(:,2),YTrain,[],'o',3)
+    end
+end
